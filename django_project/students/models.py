1dff27eac21ea906ea506589d4dd16ebd3bf548e
Vladislav1234512345 <tolerantniy2104@gmail.com>
2024-09-25 23:44:45 +0500

    Changed controllers in students/views.py to api view

---
+++ django_project/students/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=240)
+    email = models.EmailField()
+    document = models.CharField(verbose_name="Document", max_length=20)
+    phone = models.CharField(max_length=20)
+    registrationDate = models.DateField(verbose_name="Registration Date", auto_now_add=True)
+    photo = models.CharField(verbose_name="URL", max_length=512)
+
+    def __str__(self):
+        return self.name
